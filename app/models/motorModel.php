9ea8776a28726cbcd0e27f744133c09e7c762d2b
Franco Di cheronimo <d1ch33@hotmail.com>
2023-10-24 16:21:52 -0300

    elimine la imagen DER

--- app/models/motorModel.php
+++ app/models/motorModel.php
@@ -36,7 +36,8 @@ class motorModel
         $db = $this->PDO;
         /* Cambiar en las BD contraseña por password --------------------------------  */
         /* ----------------------------------------------------------------- */
-        $query = $db->prepare('INSERT INTO `usuario`(`Email`,`nombre`,`password`) VALUES (?,?,?)');
+        $query = $db->prepare('INSERT INTO `usuario`(`Email`,`nombre`,`password`) 
+                                VALUES (?,?,?)');
         $query->execute([$Email, $nombre, $password]);
         return $db->lastInsertId();
 
@@ -44,36 +45,47 @@ class motorModel
     function getUsuario($Email)
     {
         $db = $this->PDO;
-        $query = $db->prepare('SELECT * FROM `usuario` WHERE `Email`=?');
+        $query = $db->prepare('SELECT *
+                                FROM `usuario` 
+                                WHERE `Email`=?');
         $query->execute([$Email]);
         $usuario = $query->fetch(PDO::FETCH_OBJ); //usas fetch porque solo trae un dato.
         return $usuario;
     }
-/*ver mas detalles del producto*/
+    /*ver mas detalles del producto*/
     function getInfo($Id)
     {
         $db = $this->PDO;
-        $query = $db->prepare('SELECT marcas.Fabricante,productos.* FROM productos INNER JOIN marcas ON productos.Id_marca=marcas.Id WHERE productos.Id=?');
+        $query = $db->prepare('SELECT marcas.Fabricante,productos.* 
+                                FROM productos 
+                                INNER JOIN marcas 
+                                ON productos.Id_marca=marcas.Id 
+                                WHERE productos.Id=?');
         $query->execute([$Id]);
         $producto = $query->fetchAll(PDO::FETCH_OBJ);
         return $producto;
     }
-/*obtiene un producto por categoria*/
-function getProductosPorMarca($idMarca){
-    $db = $this->PDO;
-    $query = $db->prepare('SELECT * FROM productos p WHERE p.Id_marca = ?');
-    $query->execute([$idMarca]);
-    $productos = $query->fetchAll(PDO::FETCH_OBJ);
+    /*obtiene un producto por categoria*/
+    function getProductosPorMarca($idMarca)
+    {
+        $db = $this->PDO;
+        $query = $db->prepare('SELECT * 
+                                FROM productos p 
+                                WHERE p.Id_marca = ?');
+        /* p es el apodo de productos */
+        $query->execute([$idMarca]);
+        $productos = $query->fetchAll(PDO::FETCH_OBJ);
 
-    return $productos;
-}
-// filtrado por fabricante de motor
-    function getFiltroMarca($idMarca){
+        return $productos;
+    }
+    // filtrado por fabricante de motor
+    function getFiltroMarca($idMarca)
+    {
         $db = $this->PDO;
         $query = $db->prepare('SELECT *
-                            FROM productos p
-                            INNER JOIN marcas m ON p.Id_marca = m.Id
-                            WHERE m.id = ?');
+                                FROM productos p
+                                INNER JOIN marcas m ON p.Id_marca = m.Id
+                                WHERE m.id = ?');
         $query->execute([$idMarca]);
 
         // Obtengo la respuesta con un fetchAll (porque son muchos productos)
